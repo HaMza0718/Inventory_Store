ba5fe639541e14c993f1b06067a53f2b71324259
Hamza Yasir <hamzayasir677@gmail.com>
2023-02-03 22:07:59 +0500

    first commit

---
+++ inc/config/constants.php
@@ -0,0 +1,21 @@
+<?php
+	// Root url for the site
+	define('ROOT_URL', 'http://localhost/InventoryManagementSystem/');
+	
+	
+	// Database parameters
+	// Data source name
+	define('DSN', 'mysql:host=localhost;dbname=inventory_store');
+	
+	// Hostname
+	define('DB_HOST', 'localhost');
+	
+	// DB user
+	define('DB_USER', 'root');
+	
+	// DB password
+	define('DB_PASSWORD', '');
+	
+	// DB name
+	define('DB_NAME', 'inventory_store');
+?>
